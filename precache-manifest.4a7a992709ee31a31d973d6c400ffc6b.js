54d0dd2c52ed79460b8cbdd24504b05b50fba73f
lhazy111 <lhazy111@googlemail.com>
2020-07-20 10:19:24 +0100

    Updates

---
+++ precache-manifest.4a7a992709ee31a31d973d6c400ffc6b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d527c1e97250bf3c62f76a6ffb96ec",
+    "url": "/lhazy111/my-page-react/index.html"
+  },
+  {
+    "revision": "e2ee61e6e2ef24ca2a00",
+    "url": "/lhazy111/my-page-react/static/css/main.0a63fafb.chunk.css"
+  },
+  {
+    "revision": "c37c1d8fb1bfe8b0204a",
+    "url": "/lhazy111/my-page-react/static/js/2.98f91202.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/lhazy111/my-page-react/static/js/2.98f91202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2ee61e6e2ef24ca2a00",
+    "url": "/lhazy111/my-page-react/static/js/main.1f801f98.chunk.js"
+  },
+  {
+    "revision": "7ed2962cfe3ca5823d92",
+    "url": "/lhazy111/my-page-react/static/js/runtime-main.ba57a911.js"
+  },
+  {
+    "revision": "42c52669f586024988e5640eee0975c0",
+    "url": "/lhazy111/my-page-react/static/media/avataaars.42c52669.png"
+  },
+  {
+    "revision": "54e25c34664dff62408679433139b945",
+    "url": "/lhazy111/my-page-react/static/media/battleship.54e25c34.png"
+  },
+  {
+    "revision": "f53674aec54aadd8003e18c27b862066",
+    "url": "/lhazy111/my-page-react/static/media/bg_genertor.f53674ae.png"
+  },
+  {
+    "revision": "fd7894809187e4c149a786b94069e3b7",
+    "url": "/lhazy111/my-page-react/static/media/bob.fd789480.png"
+  },
+  {
+    "revision": "6ab1b207b6e1820f78c34e0c6dc6ae88",
+    "url": "/lhazy111/my-page-react/static/media/calc.6ab1b207.png"
+  },
+  {
+    "revision": "96f192a0904770f930d5dfb28a7ae23c",
+    "url": "/lhazy111/my-page-react/static/media/drummachine.96f192a0.png"
+  },
+  {
+    "revision": "91494c5b412bad11cef1cc951e3f3788",
+    "url": "/lhazy111/my-page-react/static/media/landing.91494c5b.png"
+  },
+  {
+    "revision": "a09caacee9e532642b54d00648ca9c9e",
+    "url": "/lhazy111/my-page-react/static/media/lifelong.a09caace.png"
+  },
+  {
+    "revision": "94bf2511de5bd145bdfbb8e2def98621",
+    "url": "/lhazy111/my-page-react/static/media/loulogo.94bf2511.png"
+  },
+  {
+    "revision": "5f32cb90a5c22e1b30beb978c6b89354",
+    "url": "/lhazy111/my-page-react/static/media/markup.5f32cb90.png"
+  },
+  {
+    "revision": "fae5909fa5cc222e26ff58c4e2b0b905",
+    "url": "/lhazy111/my-page-react/static/media/moriak.fae5909f.png"
+  },
+  {
+    "revision": "8d88f3f6f7bb90df3b75f10f37d6d9fa",
+    "url": "/lhazy111/my-page-react/static/media/pomodoro.8d88f3f6.png"
+  },
+  {
+    "revision": "c19e3939508f67f6f61c217b5968a84c",
+    "url": "/lhazy111/my-page-react/static/media/quotemachine.c19e3939.png"
+  },
+  {
+    "revision": "3bd04b5421b94270ef3cc24ca17d2746",
+    "url": "/lhazy111/my-page-react/static/media/s-list.3bd04b54.png"
+  },
+  {
+    "revision": "e8c1014e3cb4b13b3418292172de0b55",
+    "url": "/lhazy111/my-page-react/static/media/scratch.e8c1014e.png"
+  },
+  {
+    "revision": "07dc0425bca4bad241e67bef10e99634",
+    "url": "/lhazy111/my-page-react/static/media/trivia.07dc0425.png"
+  },
+  {
+    "revision": "59c777c9f6c2a2020502dbd043ac8ae3",
+    "url": "/lhazy111/my-page-react/static/media/weather.59c777c9.png"
+  },
+  {
+    "revision": "d30035f39fca5366eba861860b709b67",
+    "url": "/lhazy111/my-page-react/static/media/weryk.d30035f3.png"
+  }
+]);
