a69b1f3619eac533a484c095fbbbc4b58e11607d
lhazy111 <lhazy111@googlemail.com>
2022-02-13 17:32:35 +0000

    projectdata updated

--- src/pages/projects/projectsdata.js
+++ src/pages/projects/projectsdata.js
@@ -1,104 +1,111 @@
 export default [
     {
         id: 0,
+        title: 'Notes collector',
+        subtitle: 'HTML, CSS, Typescript, Semantic-UI, React, nodejs',
+        description: "Backend of the app uses AWS Lambdas and Serverless framework, API Gateway, DynamoDB and S3. Frontend web app in React consumes the backend API. It allows to create and store notes and optionally update the note uploading scan of document or any image file. It's using auth0 autentication which provide security of stored data for every user and the user can only access their data. Creation time is generetad automatically and stored in database. User also can delete the note if it is no longer required.",
+        prlink: 'https://master.d3umdzdkr27lbu.amplifyapp.com/'
+    },
+    {
+        id: 1,
         title: 'Weather page',
         subtitle: 'HTML, CSS, Bootstrap, Javascript, Jquery.',
         description: "This app uses 2 different API's: one to show exact location of the weather station, and the other to get weather report and display it. Some 250.000 records JSON file is being searched and some AJAX requests in use and DOM manipulations.",
         prlink: 'https://lhazy111.github.io/weather/'
     },
     {
-        id: 1,
+        id: 2,
         title: 'Calculator',
         subtitle: 'HTML, CSS, React Bootstrap, React',
         description: "React app done with React Hooks. Responsive design with mobile first approach thanks to React Bootstrap. One of Freecodecamp Front End Libraries Certification Projects",
         prlink: 'https://msgs3.csb.app/'
     },
     {
-        id: 2,
+        id: 3,
         title: 'Pomodoro clock',
         subtitle: 'HTML, CSS, ReactBootstrap, React',
         description: "React app done with React Hooks. The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.The technique uses a timer to break down work into intervals, traditionally 25 minutes in length, separated by short breaks. Responsive design with mobile first approach thanks to React Bootstrap. Sounds created with GarageBand and CME-PRO midi keyboard.  ",
         prlink: 'https://p7dsp.csb.app/'
     },
     {
-        id: 3,
+        id: 4,
         title: 'React Drum Machine',
         subtitle: 'HTML, CSS, ReactBootstrap, React',
         description: "React app. Responsive design with mobile first approach thanks to React Bootstrap. Set of 9 different sounds to play with.  One of Freecodecamp Front End Libraries Certification Projects",
         prlink: 'https://uh20r.csb.app/'
     },
     {
-        id: 4,
+        id: 5,
         title: 'Markup editor',
         subtitle: 'HTML, CSS, ReactBootstrap, React',
         description: 'React app. Responsive design with mobile first approach thanks to React Bootstrap. Markdown is a lightweight markup language that you can use to add formatting elements to plaintext text documents. Created by John Gruber in 2004, Markdown is now one of the world’s most popular markup languages. Day/night mode implemented, collapsable menu on small screens. ',
         prlink: 'https://01jn7.csb.app/'
     },
     {
-        id: 5,
+        id: 6,
         title: 'Quote machine',
         subtitle: 'HTML, CSS, Bootstrap, Javascript.',
         description: 'Javascript app. Responsive design with mobile first approach thanks to Bootstrap. Fetching random quote with use of quote-garden API. Allows to tweet chosen quote.',
         prlink: 'https://qhn6l.csb.app/'
     },
     {
-        id: 6,
+        id: 7,
         title: 'Bob the builder',
         subtitle: 'HTML, CSS, Javascript.',
         description: "EDX project for DEV335x course. This app is a little helper for tiler or painter (so far). It helps with some calculations required before starting building task. It also calculates the cost, coverage etc. with given walls' dimensions and product prices. Building it I went through all UX Design process starting from needfinding through scenarios, prototyping and usability tests",
         prlink: 'https://lhazy111.github.io/dev335x/'
     },
     {
-        id: 7,
+        id: 8,
         title: 'Battle ship',
         subtitle: 'HTML, CSS, Javascript',
         description: 'This app has been coded along with Headfirst "Javascript programming" book. One of the very classic games ;-)',
         prlink: 'https://lhazy111.github.io/battle-ship/'
     },
     {
-        id: 8,
+        id: 9,
         title: 'Trivia',
         subtitle: 'HTML, CSS, Bootstrap, Jquery, Javascript',
         description: "This app is a part of my learning path with edx. Some objects, arrays, localStorage and JSON operations. The aim is to choose a right answer. It allows to add new sets as well.",
         prlink: 'https://lhazy111.github.io/trivia/'
     },
     {
-        id: 9,
+        id: 10,
         title: 'Background generator',
         subtitle: 'HTML, CSS, Javascript',
         description: "This app is a little helper for web developers for creating gradient background. It allows to see the effect instantly on the screen with chosen colors. Simple site with some DOM manipulations.",
         prlink: 'https://lhazy111.github.io/background-generator/'
     },
     {
-        id: 10,
+        id: 11,
         title: 'Shopping list',
         subtitle: 'HTML, CSS, Bootstrap, Javascript',
         description: "This app is a little helper for everyone especially when using on mobile phone. It allows create shopping list on the go and then modify when shopping. Items can be deleted or strikeout. It's stripped of unnecesarry styling to make it as readable as possible even for short-sighted people.",
         prlink: 'https://lhazy111.github.io/1-2/2.html'
     },
     {
-        id: 11,
+        id: 12,
         title: 'Weryk online',
         subtitle: 'HTML, CSS, Bootstrap',
         description: "This is done as a favor for my back-end friend and as an exercise for me. Back-end done with python by him and than integrated with my front-end",
-        prlink: 'http://werykonline.com'
+        prlink: 'https://lhazy111.github.io/weryk/'
     },
     {
-        id: 12,
+        id: 13,
         title: 'Landing page',
         subtitle: 'HTML, CSS, Javascript',
         description: "This is just a landing page done for one of my food blogger friends. To be continued...hopefully",
         prlink: 'https://lhazy111.github.io/1-2/'
     },
     {
-        id: 13,
+        id: 14,
         title: 'Reaction time',
         subtitle: 'Scratch',
         description: "I made the prototype of this game when I was 14 for Commodore 64. It was very simple than ;-). This one was made as a part of HarvardX CS50 course",
         prlink: 'https://scratch.mit.edu/projects/252663326/fullscreen/'
     },
     {
-        id: 14,
+        id: 15,
         title: 'Moriak Photography',
         subtitle: 'HTML, CSS',
         description: "Actually this website has been done before I even started my learning with edx. It was on for a few years. Now only in my archives. Not too bad as for total novice that time (2009) ;-)",
